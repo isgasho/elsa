ba994d5e80425b9b674e834270e2bb56f02acbdc
busgo <248434199@qq.com>
2021-05-29 20:53:25 +0800

    add reg renew cancel

--- internal/registry/p2p/peer.go
+++ internal/registry/p2p/peer.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"context"
 	"github.com/busgo/elsa/pkg/proto/pb"
 	"google.golang.org/grpc"
 	"log"
@@ -76,15 +77,60 @@ func (pool *PeerPool) lookup() {
 // handle sync message
 func (pool *PeerPool) handleSyncMessage(message *SyncMessage) {
 
+	if len(pool.peers) == 0 {
+		return
+	}
 	switch message.Type {
 	case SyncRegType:
 		log.Printf("handle the reg sync message ....")
+		pool.reg(message.Content.(*pb.RegisterRequest))
 	case SyncRenewType:
 		log.Printf("handle the renew sync message ....")
+		pool.renew(message.Content.(*pb.RenewRequest))
 	case syncCancelType:
 		log.Printf("handle the cancel sync message ....")
+		pool.cancel(message.Content.(*pb.CancelRequest))
 	}
 
+}
+
+// sync register the service instance
+func (pool *PeerPool) reg(request *pb.RegisterRequest) {
+
+	for _, p := range pool.peers {
+		in, err := p.cli.Register(context.Background(), request)
+		if err != nil {
+			log.Printf("the sync register service instance  fail:%#v", err)
+			continue
+		}
+		log.Printf("the sync register service instance success :%#v", in)
+	}
+}
+
+// sync renew the service instance
+func (pool *PeerPool) renew(request *pb.RenewRequest) {
+
+	for _, p := range pool.peers {
+		in, err := p.cli.Renew(context.Background(), request)
+		if err != nil {
+			log.Printf("the sync renew service instance  fail:%#v", err)
+			continue
+		}
+		log.Printf("the sync renew service instance success :%#v", in)
+	}
+}
+
+// sync cancel the service instance
+func (pool *PeerPool) cancel(request *pb.CancelRequest) {
+
+	for _, p := range pool.peers {
+		in, err := p.cli.Cancel(context.Background(), request)
+		if err != nil {
+			log.Printf("the sync cancel service instance  fail:%#v", err)
+			continue
+		}
+		log.Printf("the sync cancel service instance success :%#v", in)
+	}
 }
 
 // new a peer with endpoint
